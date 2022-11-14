de8e03b1c35d4656e4573cb1ab2b16ebb9f0f207
Nathan Page <33336363+ndpage@users.noreply.github.com>
2022-11-13 22:08:02 -0500

    Assert is used to check conditions and break if condition is false

---
+++ C-structured-programming/week1/assert.c
@@ -0,0 +1,36 @@
+/**
+ * @file assert.c
+ * 
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-13
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdio.h>
+#include <assert.h>
+int main()
+{
+
+  // basic assert expression
+  float x = 0;
+  float y = 0;
+  
+  // True condition
+  printf("Program will run is x is greater than or equal to y\n\n");
+  // prompt for input
+  printf("Enter two floats: ");
+  scanf("%f %f", &x, &y);
+  printf("\n");
+  
+  // check condition
+  assert(x >= y);
+
+  printf("%0.2f is great than %0.2f\n", x, y);
+  printf("Program ran successfully!\n");
+
+  return 0;
+}
