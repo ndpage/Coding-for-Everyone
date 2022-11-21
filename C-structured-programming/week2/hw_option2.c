f25409abdab450f4e62a9f888eb1d563de000ad0
Nathan Page <33336363+ndpage@users.noreply.github.com>
2022-11-21 14:07:56 -0500

    Add inital code for week2 assignement option 2

---
+++ C-structured-programming/week2/hw_option2.c
@@ -0,0 +1,37 @@
+/**
+ * @file hw_option2.c
+ * 
+ * @author Nathan Page
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-21
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "hw_option2.h"
+
+int main(void){
+  return 0;
+}
+
+// Functions definitions
+employee randEmployee(){
+  // declare new employee struct
+  employee em;
+  
+  dept rand_dept = (dept)3;
+  em.annual_salary = deptSalaryGen(rand_dept);
+  em.department = rand_dept;
+  em.social_security_number = genSocialSecurityNum();
+  return em;
+}
+
+int deptSalaryGen(dept department){
+  return 0;
+}
+
+unsigned int genSocialSecurityNum(){
+  return 0;
+}
---
+++ C-structured-programming/week2/hw_option2.h
@@ -0,0 +1,30 @@
+/**
+ * @file hw_option2.h
+ * @author Nathan Page
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-21
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+// Include necesssary libraries
+#include <stdio.h>
+
+int main(void); // main function declaration
+
+// The enum is the type for department
+typedef enum {HR, Sales, Research, Software, Executive} dept;
+
+// employee type definition struct
+typedef struct {
+  dept department;
+  int annual_salary;
+  unsigned int social_security_number;
+} employee;
+
+// Functions declaration
+employee randEmployee();
+int deptSalaryGen(dept department);
+unsigned int genSocialSecurityNum();
