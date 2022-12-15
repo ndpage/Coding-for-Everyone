a64efd9112b21b78d62ed4422ee5e77d229f27af
Nathan Page <33336363+ndpage@users.noreply.github.com>
2022-12-14 21:57:17 -0500

    randNum bug fix and comments

--- C-structured-programming/week2/hw_option2.c
+++ C-structured-programming/week2/hw_option2.c
@@ -10,11 +10,14 @@
  *
  */
 
-#include "hw_option2.h"
+#include "hw_option2.h" // include header file
 
+// Main entry point of program
 int main(void)
 {
-  // declare new employees and generate random data
+  printf("%d\n", randNum(5));
+  printf("%d\n", randNum(4));
+  // declare new employees and generate random data for each one
   employee Jim  = randEmployee();
   employee Sarah  = randEmployee();
   employee Allen  = randEmployee();
@@ -26,37 +29,43 @@ int main(void)
   employee Dan  = randEmployee();
   employee Ben  = randEmployee();
 
+  // Print each employee data and name
   printEmployee(Jim,"Jim");
+  printEmployee(Sarah,"Sarah");
+  printEmployee(Allen,"Allen");
+  printEmployee(Jerry,"Jerry");
+  printEmployee(Mike,"Mike");
+  printEmployee(Jessica,"Jessica");
+  printEmployee(Karen,"Karen");
+  printEmployee(Nick,"Nick");
+  printEmployee(Dan,"Dan");
+  printEmployee(Ben,"Ben");
 
   return 0;
 }
 
 /**
- * @brief Function to print employee information
+ * @brief randEmployee() generates an employee data with random department, salary, and social security number
  * 
- * @param em 
- * @param name 
+ * @return employee 
  */
-void printEmployee(employee em, char* name){
-  printf("%s's salary:                 %d\n", name, em.annual_salary);
-  printf("%s's department:             %s\n", name, printDept(&em));
-  printf("%s's social security number: %u\n", name, em.social_security_number);
-  printf("\n");
-}
-
-// Functions definitions
 employee randEmployee()
 {
   // declare new employee struct
   employee em;
 
-  dept rand_dept = (dept)(randNum(3));
+  dept rand_dept = (dept)(randNum(4));
   em.annual_salary = deptSalaryGen(rand_dept);
   em.department = rand_dept;
   em.social_security_number = genSocialSecurityNum();
   return em;
 }
-
+/**
+ * @brief deptSalaryGen generates a random salary based on the depart passed as the argument
+ * 
+ * @param department 
+ * @return int 
+ */
 int deptSalaryGen(dept department)
 {
   int base;
@@ -97,7 +106,11 @@ int deptSalaryGen(dept department)
 
   return base + variable;
 }
-
+/**
+ * @brief genSocialSecurityNum will generate a pseudo-random social security number 9 digits long
+ * 
+ * @return unsigned int 
+ */
 unsigned int genSocialSecurityNum()
 {
   char *num_str = "";
@@ -113,13 +126,25 @@ unsigned int genSocialSecurityNum()
   return (unsigned int)num_str;
 }
 
+/**
+ * @brief randNum generates a pseudo-random number up to the max passed as the argument
+ * @example randNum(5) - Generates a random number between 0 to 5
+ * @param max 
+ * @return int 
+ */
 int randNum(int max)
 {
   time_t t1;
   srand(time(&t1) + rand());
-  return rand() % max + 1;
+  return rand() % (max + 1);
 }
 
+/**
+ * @brief printDept prints the department of the enum dept in text format
+ * 
+ * @param em 
+ * @return char* 
+ */
 char *printDept(employee *em)
 {
   switch (em->department)
@@ -155,4 +180,18 @@ char *printDept(employee *em)
     break;
   }
   }
+}
+
+/**
+ * @brief Function to print employee information
+ * 
+ * @param em 
+ * @param name 
+ */
+void printEmployee(employee em, char* name){
+  printf("------------------------------------------\n");
+  printf("%s's salary:                 %d\n", name, em.annual_salary);
+  printf("%s's department:             %s\n", name, printDept(&em));
+  printf("%s's social security number: %u\n", name, em.social_security_number);
+  printf("------------------------------------------\n");
 }
