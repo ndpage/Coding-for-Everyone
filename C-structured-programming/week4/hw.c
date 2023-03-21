7791052d08e04d84c1ad36ffebec69f5fd2065e3
Nathan Page <33336363+ndpage@users.noreply.github.com>
2023-03-20 21:45:42 -0400

    Add file for week 4 homework

---
+++ C-structured-programming/week4/hw.c
@@ -0,0 +1,19 @@
+/**
+ * @file hw.c
+ * @author Nathan Page
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-20
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+
+  return 0;
+}
