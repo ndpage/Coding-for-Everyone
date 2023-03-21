6c6fb78e1a72d41c50818ca85421c78c0aed0675
Nathan Page <33336363+ndpage@users.noreply.github.com>
2023-03-20 21:53:49 -0400

    update fileio to have output file as optional

--- C-structured-programming/week4/fileio.c
+++ C-structured-programming/week4/fileio.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /**
  * @brief main entry point of program
@@ -9,10 +10,15 @@
  * @return int 
  */
 int main(int argc, char* argv[]){
-  if(argc < 3){
-    printf("Missing arguments: Please specify input and output file names\n");
+  if(argc < 2){
+    printf("Missing arguments: Please specify input file and output file (optional) \n");
     exit(1);
   }
+  bool output = true;
+  if(argc == 2){
+    output = false;
+  }
+
   FILE *inpfile, *outfile;
   char c;
   int size = 0;
@@ -23,16 +29,22 @@ int main(int argc, char* argv[]){
     printf("Input file handle is NULL");
     exit(1);
   }
-  outfile = fopen(argv[2], "w+");
+  if(output){
+    outfile = fopen(argv[2], "w+");
+  }
   while ((c = fgetc(inpfile)) != EOF) {
         printf("%c", c);
-        fputc(c, outfile);
+        if(output){
+          fputc(c, outfile);
+        }
         size++;
   }
   printf("\n");
-
+  printf("Size: %d\n", size);
   // close file references/pointers
   fclose(inpfile);
-  fclose(outfile);
-
+  if(output){
+    fclose(outfile);
+  }
+  return 0;
 }
